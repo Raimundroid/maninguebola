52d8e17b5c6d292035be9a20bfdf99bb746b26da
Raimundroid <raimundroid@gmail.com>
2025-11-17 16:21:47 +0200

    test commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Test Test</h2>
+      <h2>Just a Test</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
