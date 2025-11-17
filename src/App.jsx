93a667cf6b97e62845f7e0316731c32ffea5cb63
Raimundroid <raimundroid@gmail.com>
2025-11-17 17:07:06 +0200

    deleted the default presentaion page

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,6 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
       <h2>Just a Test</h2>
       <div className="card">
@@ -28,7 +29,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <h2>⚽ManingueBola⚽</h2>
+      <p>... coming soon...</p>
     </>
   );
 }
