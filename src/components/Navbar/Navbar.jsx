f2721854c9d551a92bd9cab7ca3bb9c48f127da4
Raimundroid <raimundroid@gmail.com>
2025-11-19 15:06:56 +0200

    installed lucide-react, deleted the demo navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// import React from "react";
+// import { Link } from "react-router-dom";
 
-const Navbar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Início</Link>
-        </li>
+// const Navbar = () => {
+//   return (
+//     <nav>
+//       <ul>
+//         <li>
+//           <Link to="/">Início</Link>
+//         </li>
 
-        <li>
-          <Link to="/jogos">Jogos</Link>
-        </li>
+//         <li>
+//           <Link to="/jogos">Jogos</Link>
+//         </li>
 
-        <li>
-          <Link to="/jogadores">Jogadores</Link>
-        </li>
+//         <li>
+//           <Link to="/jogadores">Jogadores</Link>
+//         </li>
 
-        <li>
-          <Link to="/classificacao">Classificação</Link>
-        </li>
+//         <li>
+//           <Link to="/classificacao">Classificação</Link>
+//         </li>
 
-        <li>
-          <Link to="/estatistica">Estatística</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+//         <li>
+//           <Link to="/estatistica">Estatística</Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// };
 
-export default Navbar;
+// export default Navbar;
+
+//  const navItems = [
+//    { id: "home", path: "/", label: "Início", icon: "🏠" },
+//    { id: "matches", path: "/jogos", label: "Jogos", icon: "⚽" },
+//    {
+//      id: "standings",
+//      path: "/classificacao",
+//      label: "Classificação",
+//      icon: "🏆",
+//    },
+//    {
+//      id: "ststistics",
+//      path: "/estatistica",
+//      label: "Estatística",
+//      icon: "📊",
+//    },
+//    { id: "teams", path: "/teams", label: "Equipas", icon: "👥" },
+//    { id: "players", path: "/jogadores", label: "Jogadores", icon: "⭐" },
+//  ];
