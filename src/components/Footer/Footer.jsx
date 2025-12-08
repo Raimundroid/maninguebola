9c832ea877a6e119b3075f5db631c85cddc3d054
Raimundroid <raimundroid@gmail.com>
2025-12-08 14:06:05 +0200

    Footer.css: complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,29 +3,6 @@
 // // ============================================
 // // Site footer with copyright and links
 
-// import React from "react";
-// import "./Footer.css";
-
-// const Footer = () => {
-//   return (
-//     <footer className="footer">
-//       <div className="footer-content">
-//         <p>
-//           <strong>⚽ManingueBola</strong> - Acompanhe o Futebol Local
-//         </p>
-
-//         <p>&copy; 2025 &#8226; Todos os Direitos Reservados</p>
-
-//         <p>
-//           <a href="#">Contacto</a> • <a href="#">Sobre</a> •{" "}
-//           <a href="#">Privacidade</a>
-//         </p>
-//       </div>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Footer.css";
