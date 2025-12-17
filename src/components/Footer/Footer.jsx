545fa3d12d3d102aa44e1b8982d180aac1beaa6b
Raimundroid <raimundroid@gmail.com>
2025-12-17 15:11:11 +0200

    added ManingueBola Facebook page to the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -51,8 +51,8 @@ const Footer = () => {
         {/* 3. Social Media & Contact */}
         <div className="footer-socials">
           {/* Facebook Icon */}
-          {/* <a
-            href="https://facebook.com"
+          <a
+            href="https://www.facebook.com/profile.php?id=61584955900976"
             target="_blank"
             rel="noreferrer"
             className="social-link"
@@ -61,7 +61,7 @@ const Footer = () => {
             <svg width={20} height={20} viewBox="0 0 24 24" fill="currentColor">
               <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
             </svg>
-          </a> */}
+          </a>
 
           {/* WhatsApp Icon */}
           <a
