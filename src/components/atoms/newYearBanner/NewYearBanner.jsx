8057e1390c7c134e6d1e257c41498b34bc379aa5
Raimundroid <raimundroid@gmail.com>
2026-01-01 09:29:44 +0200

    network test commit 2

--- src/components/atoms/newYearBanner/NewYearBanner.jsx
+++ src/components/atoms/newYearBanner/NewYearBanner.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import "./NewYearBanner.css";
 
 const NewYearBanner = () => {
-  return <div>Feliz Ano Novo</div>;
+  // Get today's date from the user's browser (client-side only, perfect for  static websites)
+  const today = new Date();
+
+  // Extract month and day
+  const month = today.getMonth(); // January = 0
+
+  const day = today.getDate(); // 1 - 31
+
+  // Automatically calculate the current year (future-proof)
+  const year = today.getFullYear();
+
+  //   define the visible date range: Jan 1 - Jan 15
+  const isNewYaerPeriod = month === 0 && day >= 1 && day <= 15;
+
+  //   Component always exists, but conditionally renders nothing if outside date range
+  if (!isNewYaerPeriod) return null;
+
+  return (
+    // Semantic <section> improves accessibility and documnent structure
+    <section
+      className="new-year-banner"
+      aria-label={`Banner the celebração de ano novo de ${year}`}
+    >
+      <div className="new-year-banner__overlay">
+        {/* Heading uses h2 because it lives under the main hero section */}
+        <h2 className="new-year-banner__title">🎉 Feliz Ano Novo {year} 🎉</h2>
+        <p className="new-year-banner__text">
+          Muitos sucessos, saúde, paz, e alegria para você e sua família! Que{" "}
+          {year} seja um ano de muitas conquistas e sonhos realizado.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default NewYearBanner;
